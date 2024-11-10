a55722b2fc4b496b666aa8e27609a3f708de05a0
JustinChing30 <151497702+JustinChing30@users.noreply.github.com>
2024-11-10 18:33:29 +0800

    removed comma formatting

--- MCO1/MP_2_Kotlin.kt
+++ MCO1/MP_2_Kotlin.kt
@@ -31,7 +31,7 @@ fun main() {
 
     println("\nLoan Amount: PHP $loan")
     println("Annual Interest Rate: $rate%")
-    println("Loan Term: ${"%,.0f".format(loanMonths)} months")
-    println("Monthly Repayment: PHP ${"%,.2f".format(repayment)}")
-    println("Total Interest: PHP ${"%,.2f".format(totalInterest)}")
+    println("Loan Term: ${"%.0f".format(loanMonths)} months")
+    println("Monthly Repayment: PHP ${"%.2f".format(repayment)}")
+    println("Total Interest: PHP ${"%.2f".format(totalInterest)}")
 }
